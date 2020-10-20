3bb6159ca81e75956f0341c86d35ffbb8e089475
GitHub Action <action@github.com>
2020-10-20 06:37:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/notexx/intellijplatformplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/notexx/intellijplatformplugin/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.notexx.intellijplatformplugin.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.notexx.intellijplatformplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/notexx/intellijplatformplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.notexx.intellijplatformplugin.services
+
+import com.github.notexx.intellijplatformplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
